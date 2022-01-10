c48c0ea4f6a1d904fad38b7e3f4ae57c0d22686b
RafaelCunha136 <raffazcunha@gmail.com>
2022-01-10 16:08:07 -0300

    Adicionado Calculadora Básica e Testes

---
+++ ConsoleApp1/ConsoleApp1/CalculadoraBasica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CalculadoraBasica
+    {
+        public int Soma(int a, int b)
+            {
+                return a + b; 
+            }
+        public int Subtracao(int a, int b)
+            {
+            return a - b;
+            }
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1Tests/CalculadoraBasicaTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class CalculadoraBasicaTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var calculadora = new CalculadoraBasica();
+
+            var result = calculadora.Soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var calculadora = new CalculadoraBasica();
+
+            var result = calculadora.Subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+            
+        }
+
+        [TestMethod()]
+        public void MultiplicacaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var calculadora = new CalculadoraBasica();
+
+            var result = calculadora.Multiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+            
+        }
+
+        [TestMethod()]
+        public void DivisaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var calculadora = new CalculadoraBasica();
+
+            var result = calculadora.Divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+            
+        }
+    }
+}
