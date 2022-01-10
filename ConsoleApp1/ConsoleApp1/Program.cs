22ae198e647314bb30e086c0035eb585da782298
RafaelCunha136 <raffazcunha@gmail.com>
2022-01-10 15:16:15 -0300

    Push do Visual Studio direto para o GitHub

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World!";
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
